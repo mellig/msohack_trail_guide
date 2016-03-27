a5220116c834a672348b560ee44d5c5eb556fc65
eidietrich <eric.dietrich@gmail.com>
2016-03-27 14:56:05 -0600

    Add documentation

--- README.md
+++ README.md
@@ -2,9 +2,51 @@
 
 Attempt to use public data on Missoula-area trails to create a simple interactive website. Work in progress.
 
+Intent is to adress a percieved gap in currently available online trail information/mapping, most of which seems to be either produced originally for print (and uploaded to the web as pdfs/static images), or produced at a national scale without much locally-focused attention. Mobile usability is also an issue with some existing services.
+
 raw-data folder includes raw .shp files
 data folder includes geojson files for import into mapping via leaflet
 CRS is WGS84/EPSG:4326
 
 Site designed with Foundation 5.
 
+## TODOs:
+
+Refine interactive map behavior:
+
+Have trail system organized by region (display colors based on that?)
+On click, zoom to area of particular trail system and highlight (make other trails less opaque)
+
+Have ways to highlight portions of the trail network by characteristic (e.g. leash laws)
+
+Sync map to filtering behavior to the category buttons on home page (e.g. to make it possible to select trails by leash law category)
+
+Data work:
+- Clean up geodata (e.g. remove small sections of trail in town)
+- Add in missing trailheads
+- Figure out how to present length with multi-segment trail networks
+
+See if it's possible to get cleaner data (with more information) from the city or other entities directly.
+
+## Other notes:
+
+One interface idea:
+Presenting users with an initial, pre-selected 'Top 5 hikes' upon first reaching the site (e.g. classic local day hikes like the M trail), then let users select other hikes based on the interface.
+
+Other ideas we discussed:
+- Adding elevation profiles
+- Outside link to Google Maps providing directions to the trailhead
+
+Useful information to include for each trail:
+- Trail name
+- Length elevation gain
+- Seasonal use restrictions 
+- Allowed uses (e.g. bikes, motor-vehicles, horses)
+- Leash laws
+- How many dogs actually use the trail (for people who want to avoid them for whatever reason)
+- Type of surface / ADA-accessibility
+- Hike descriptions
+- Trailhead facilities
+- Camping availability
+- Entity responsible for maintenance
+- What's worth seeing?
--- js/main-map-generator.js
+++ js/main-map-generator.js
@@ -1,13 +1,31 @@
 /**
-Uses Leaflet to create an embedded slippy map attached to a #map element.
+Uses Leaflet.js to create an embedded slippy map attached to a #map element.
 */
 
 // Leaflet documentation: http://leafletjs.com/reference.html
 // maps.stamen.com library used for basemap
 // leaflet.ajax used to load external geojson file
 
-// Create Leafley map object, set view to center over Missoula
+// GLOBAL VARIABLES
 
+// TODO: Figure out how to make this something that can be dynamically changed based on data properties
+// TODO: Figure out how to adjust these based on the overall map zoom level (e.g., making trailhead markers bigger at higher zooms)
+var trailheadMarkerStyle = {
+    radius: 4,
+    fillColor: "#e6550d",
+    color: "#fff",
+    weight: 1.2,
+    opacity: 1,
+    fillOpacity: 1
+};
+
+// Array to store layers for each feature type
+// TODO: Implement the functionality that uses this
+var trailAreas = [];
+
+// CREATE MAP / BASEMAP
+
+// Create Leafley map object, set view to center over Missoula, MT
 var trailMap = L.map('map', {
   center: [46.84,-114.03],
   zoom: 10,
@@ -19,21 +37,7 @@ var trailMap = L.map('map', {
 var basemap = new L.StamenTileLayer("terrain");
 trailMap.addLayer(basemap);
 
-//array to store layers for each feature type
-var trailAreas = [];
-
-
-// Trailhead marker style
-// TODO: Figure out how to make this something that can be dynamically changed based on data properties
-// TODO: Figure out how to adjust these based on the overall map zoom level (e.g., making trailhead markers bigger at higher zooms)
-var trailheadMarkerStyle = {
-    radius: 4,
-    fillColor: "#e6550d",
-    color: "#fff",
-    weight: 1.2,
-    opacity: 1,
-    fillOpacity: 1
-};
+// ADD TRAIL & TRAILHEAD DATA
 
 // Load trail network data
 var trails = new L.GeoJSON.AJAX("data/msa-trails-all-uncleaned.geojson", {
@@ -56,8 +60,12 @@ var trailheads = new L.GeoJSON.AJAX("data/msa-trailheads.geojson", {
 trails.addTo(trailMap);
 trailheads.addTo(trailMap);
 
-// Set event listeners
+// SET EVENT LISTENERS
+
+// For zoom in effect on clicking a trail network
 trails.on('click', zoom);
+
+// HELPER FUNCTIONS
 
 // Sorts features into groups by park name
 function onEachFeature(feature, layer) {
@@ -72,7 +80,7 @@ function onEachFeature(feature, layer) {
 }
 
 function zoom(e) {
-  // Zoom to M network
+  // Former functionality, zooming to M trail network
   //var latlng = new L.LatLng(46.862, -113.980);
 
   // Zoom to point clicked
@@ -84,9 +92,17 @@ function zoom(e) {
   //TODO: Figure out how to expand the clickable are so you don't have to hit the trail feature directly to zoom into the area
 }
 
-
 /**
 Desired behavior through here:
+
 Have trail system organized by region/park (display colors based on that?)
 On click, zoom to area of particular trail system and highlight (make other trails less opaque)
- */
+
+Have ways to highlight portions of the trail network by characteristic (e.g. leash laws)
+
+Sync map to filtering behavior for the
+
+OTHER TODOS:
+Update to use
+
+*/
